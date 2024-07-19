26fac3a705fce82b3ee6e113a6e2e5f8f2685b06
Ian Davidson <Ianmattdavidson@gmail.com>
2024-07-18 20:06:40 -0700

    clean up before merging this branch in

--- src/main/java/com/ian/davidson/port/scanner/transformer/TenantTransformer.java
+++ src/main/java/com/ian/davidson/port/scanner/transformer/TenantTransformer.java
@@ -13,16 +13,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class TenantTransformer {
 
-//    private final AddressTransformer addressTransformer;
-//    private final PortTransformer portTransformer;
-//
-//    public TenantTransformer(final AddressTransformer addressTransformer,
-//                             final PortTransformer portTransformer) {
-//        this.addressTransformer = addressTransformer;
-//        this.portTransformer = portTransformer;
-//    }
-
-
     public Tenant toTenant(final TenantRequest tenantRequest) {
         //Used in context
         return Tenant.builder()
@@ -30,21 +20,15 @@ public class TenantTransformer {
                 .build();
     }
 
-//    public Tenant toTenant(final TenantRequest tenantRequest){
-//        Set<Address> addresses = addressTransformer.toAddresses(tenantRequest.addresses());
-//        Set<Port> ports = portTransformer.toPorts(tenantRequest.ports());
-//
-//        return Tenant.builder()
-//                .name(tenantRequest.name())
-//                .addresses(addresses)
-//                .ports(ports)
-//                .build();
-//    }
-
     public TenantResponse toTenantResponse(final Tenant tenant) {
 
-        Set<Integer> ports = tenant.getPorts().stream().map(Port::getPort).collect(Collectors.toSet());
-        Set<String> addresses = tenant.getAddresses().stream().map(Address::getAddress).collect(Collectors.toSet());
+        Set<Integer> ports = tenant.getPorts() == null
+                ? new HashSet<>()
+                : tenant.getPorts().stream().map(Port::getPort).collect(Collectors.toSet());
+
+        Set<String> addresses = tenant.getAddresses() == null
+                ? new HashSet<>()
+                : tenant.getAddresses().stream().map(Address::getAddress).collect(Collectors.toSet());
 
         return TenantResponse.builder()
                 .id(tenant.getId())
